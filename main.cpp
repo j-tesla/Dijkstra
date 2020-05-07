00ee563f85b9035751f40dfe251d4d7abab78004
j-tesla <58279279+j-tesla@users.noreply.github.com>
2020-05-08 01:23:38 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+
+class Graph {
+
+public:
+    static void
+
+private:
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+
+    return 0;
+}
