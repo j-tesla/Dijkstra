da1b340d77f5cde8261f3dad7019e2b619a98c4b
j-tesla <58279279+j-tesla@users.noreply.github.com>
2020-05-08 06:12:13 +0530

    07 may

--- main.cpp
+++ main.cpp
@@ -1,14 +1,66 @@
 #include <iostream>
 #include <random>
+
 using namespace std;
-
-
+/*                                                                                                      // fixit prob
+default_random_engine generator;
+uniform_real_distribution<float> distribution(0,1);
+float prob
+*/
 class Graph {
 
 public:
-    static void
+
+    // constructors:
+    Graph(int size): size(size) {
+        edges = new int*[size];
+
+        for (int i = 0; i < size; ++i) {
+            edges[i] = new int[size];
+        }
+        for (int i = 0; i < size; ++i) {
+            edges[i][i] = 0;
+        }
+    }
+    Graph(int size, float density, int distance_range): size(size) {
+        edges = new int*[size];
+
+        srand(time(0));         // seed time
+        for (int i = 0; i < size; ++i) {
+            edges[i] = new int[size];
+        }
+        for (int i = 0; i < size; ++i) {
+            for (int j = i; j < size; ++j) {
+                if (i == j) edges[i][j] = 0;
+                else edges[i][j] = edges[j][i] = (static_cast<int>(prob() < 0.19)) * (rand()%distance_range + 1) // fixit prob
+            }
+        }
+    }
+
+
+    int E () {                              // returns the number of edges in the graph
+        int count = 0;
+        for (int i = 0; i < size; ++i) {
+            for (int j = i + 1; j < size; ++j) {
+                if (this->edges[i][j] > 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    int V () {                              // returns number of vertices in the graph
+        return this->size;
+    }
 
 private:
+    int size;
+    int** edges;
+    /*
+     * THE MATRIX REPRESENTATION OF GRAPH:
+     *      edges[i][j] is distance between nodes i and j if directly connected or 0 if not connected directly
+     */
 
 };
 
@@ -18,3 +70,6 @@ int main() {
 
     return 0;
 }
+
+
+// learn inheritance
